b11af92c6069a35a9c3f78ea829f76ae3b32a302
RhodyArthur <rhodaarthur48@gmail.com>
2025-10-22 09:23:35 +0000

    feat: add main functionality to analyze book text

---
+++ main.py
@@ -0,0 +1,26 @@
+from stats import get_num_words
+from stats import get_num_chars
+from stats import print_sorted_report
+
+def get_book_text(path_to_file):
+    try:
+        with open(path_to_file) as f:
+            file_contents = f.read()
+        return file_contents
+    except FileNotFoundError:
+        raise FileNotFoundError("File not found: " + path_to_file)
+    
+
+def main():
+    book_string = get_book_text('books/frankenstein.txt')
+    
+    print (f"""
+    ============ BOOKBOT ============
+    Analyzing book found at books/frankenstein.txt...
+    ----------- Word Count ----------
+    {get_num_words(book_string)}
+    --------- Character Count -------
+    {print_sorted_report(get_num_chars(book_string))}
+    ============= END ===============
+    """)
+main()
